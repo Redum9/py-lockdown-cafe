de06bac6c8965240a19d1bd06e5aa58717e27b84
Redum9 <dumaneckijdenis@gmail.com>
2026-01-22 14:25:12 +0200

    resolved the task!

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
-from app.errors import VaccineError
-from app.errors import NotWearingMaskError
+from app.errors import VaccineError,NotWearingMaskError
 from app.cafe import Cafe
 
 def go_to_cafe(friends: list, cafe: Cafe) -> str:
